d7f451ca7cdb73894b96d6f94119914e00641ab7
Jihyuk Kim <jihyukkim@yonsei.ac.kr>
2021-11-23 21:23:56 +0900

    .

--- meta_script-open_set.sh
+++ meta_script-open_set.sh
@@ -2,9 +2,6 @@
 wget https://www.dropbox.com/s/lgeza7owhn9dwtu/Processed_data_for_onmt.zip?dl=1
 unzip Processed_data_for_onmt.zip\?dl\=1 
 rm Processed_data_for_onmt.zip\?dl\=1 
-# data/Processed_data_for_onmt/Training/word_kp20k_training_context_filtered.txt
-# data/Processed_data_for_onmt/Training/word_kp20k_training_context_nstpws_sims_retrieved_keyphrases_filtered.txt
-# data/Processed_data_for_onmt/Training/word_kp20k_training_keyword_filtered.txt
 
 mkdir -p data/kp20k_filtered
 # train
@@ -20,6 +17,8 @@ cp data/Processed_data_for_onmt/Testing/word_kp20k_testing_context.txt data/kp20
 cp data/Processed_data_for_onmt/Testing/word_kp20k_testing_context_nstpws_sims_retrieved_keyphrases_filtered.txt data/kp20k_filtered/test_ret.txt
 cp data/Processed_data_for_onmt/Testing/word_kp20k_testing_keyword.txt data/kp20k_filtered/test_trg.txt
 
+rm -r data/Processed_data_for_onmt*
+
 # Preprocessing
 # w/o title
 cd scientific_publications
@@ -31,7 +30,9 @@ python preprocess.py -data_dir ../data/kp20k_filtered_title -vocab_size 50000 -u
 cd ../
 
 # run experiment
+cd scientific_publications
 # w/o title
 source run.sh 0 3 
 # w/ title
 source run.sh 0 3 title
+cd ../
